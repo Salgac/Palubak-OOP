89748066ce344af5e5d8e2ab648ada6873eb0295
Dominik <domco.salgovic@gmail.com>
2020-03-25 13:06:45 +0100

    Added script for destination setting

--- src/pp/visual/buttons/scripts/functions/DestinationSetScript.java
+++ src/pp/visual/buttons/scripts/functions/DestinationSetScript.java
@@ -3,29 +3,42 @@ package pp.visual.buttons.scripts.functions;
 import pp.visual.buttons.BuseButton;
 import pp.visual.buttons.scripts.helper.FUNCTION_TYPE;
 
-public class DestinationSetScript extends BuseScript {
+public class DestinationSetScript extends BuseScript implements StagedScript {
 
     public DestinationSetScript(BuseButton button) {
-        super(button, FUNCTION_TYPE.LINE_SET);
+        super(button, FUNCTION_TYPE.DESTINATION_SET);
     }
 
     @Override
     public void execute() {
+        button.getData().setActiveScript(this);
+        firstStage();
+    }
 
+    private void firstStage() {
+        this.text = button.getData().getCurrent().getDestination();
+        refreshText();
+    }
+
+    private void endStage() {
+        getTextLine(0).setText("");
+        button.getData().setActiveScript(null);
+        button.getData().setInputModeActive(false);
     }
 
     @Override
     public void refreshText() {
-
+        getTextLine(0).setText("Ciel: >" + this.text + "<");
     }
 
     @Override
     void accept() {
-
+        button.getData().getCurrent().setDestination(text);
+        endStage();
     }
 
     @Override
     void cancel() {
-
+        endStage();
     }
 }
